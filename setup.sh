79cb7f2318a4a15ea8d412076501acd1678f3fd1
iyper <70619786+iyper@users.noreply.github.com>
2023-09-23 15:36:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="testml"
+GROUP="az-ml-lab-rvi"
 LOCATION="eastus"
-WORKSPACE="azureml"
+WORKSPACE="az-gl-lab-wksp-rvi"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
